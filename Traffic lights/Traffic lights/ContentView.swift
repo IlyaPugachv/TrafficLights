24d4372331c27c2c2e83ccf5a8bdd979d7f41900
Ilya Pugachev <ilyapugachv@gmail.com>
2023-12-08 13:44:50 +0300

    Project created

---
+++ Traffic lights/Traffic lights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Traffic lights
+//
+//  Created by Илья Пугачёв on 8.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Traffic lights/Traffic lights/Traffic_lightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_lightsApp.swift
+//  Traffic lights
+//
+//  Created by Илья Пугачёв on 8.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_lightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
