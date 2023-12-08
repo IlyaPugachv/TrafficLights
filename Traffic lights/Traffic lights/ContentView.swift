2208947b7ef6e5ace83e0281f19675e2ef34b550
Ilya Pugachev <ilyapugachv@gmail.com>
2023-12-08 14:34:21 +0300

    Project created

--- Traffic lights/Traffic lights/ContentView.swift
+++ Traffic lights/Traffic lights/ContentView.swift
@@ -1,24 +1,63 @@
-//
-//  ContentView.swift
-//  Traffic lights
-//
-//  Created by Илья Пугачёв on 8.12.23.
-//
-
 import SwiftUI
 
+enum TrafficLightState {
+    case red
+    case yellow
+    case green
+}
+
 struct ContentView: View {
+    @State private var lightState = TrafficLightState.red
+    @State private var buttonText = "Next"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .foregroundColor(.red)
+                .opacity(lightState == .red || lightState == .yellow ? 1 : 0.3)
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            Circle()
+                .foregroundColor(.yellow)
+                .opacity(lightState == .yellow || lightState == .yellow ? 1 : 0.3)
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            Circle()
+                .foregroundColor(.green)
+                .opacity(lightState == .green ? 1 : 0.3)
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            Button(action: {
+                self.nextLightState()
+            }, label: {
+                Text(buttonText)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func nextLightState() {
+        switch lightState {
+        case .red:
+            lightState = .yellow
+            buttonText = "Next"
+        case .yellow:
+            lightState = .green
+        case .green:
+            lightState = .red
+            buttonText = "Start"
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
